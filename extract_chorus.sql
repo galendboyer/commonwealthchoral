9aac00a30beeeec895ef82c006fe7dccaff669f7
Galen Boyer <gboyer@olin.edu>
2025-09-16 23:35:49 -0400

    Loaded single with no mapping layer needed

--- extract_chorus.sql
+++ extract_chorus.sql
@@ -1,1 +1,14 @@
-SELECT * FROM v_chorus WHERE email = 'galendboyer@gmail.com'
+SELECT
+        email
+,       Last
+,       First
+,       Middle
+,       "Voice Part" 
+,       "Paid Staff"
+,       Active
+,       Address1
+,       Address2
+,       City
+,       State AS "State/Province"
+,       ZIP AS "Zip/Postal Code"
+FROM v_chorus WHERE email = 'galendboyer@gmail.com'
