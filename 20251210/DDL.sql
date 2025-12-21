8ce931d74757f362c717c7d65514898b4c141a15
Galen Boyer <gboyer@olin.edu>
2025-12-20 20:35:49 -0500

    ...

---
+++ 20251210/DDL.sql
@@ -0,0 +1,98 @@
+DROP TABLE IF EXISTS t_Volunteer_Responses
+go
+CREATE TABLE t_Volunteer_Responses
+(
+  Timestmp       VARCHAR(MAX)
+, Email          VARCHAR(MAX)
+, Full_Name      VARCHAR(MAX)
+, LName          VARCHAR(MAX)
+, FName          VARCHAR(MAX)
+, Occupation     VARCHAR(MAX)
+, Retired        VARCHAR(MAX)
+, Capabilities   VARCHAR(MAX)
+, CT_Ads         VARCHAR(MAX)
+, CT_Board       VARCHAR(MAX)
+, CT_Concerts    VARCHAR(MAX)
+, CT_Development VARCHAR(MAX)
+, CT_Finance     VARCHAR(MAX)
+, CT_Hospitality VARCHAR(MAX)
+, CT_Investment  VARCHAR(MAX)
+, CT_Marketing   VARCHAR(MAX)
+, CT_Membership  VARCHAR(MAX)
+, CT_Outreach    VARCHAR(MAX)
+, CT_ProgramBook VARCHAR(MAX)
+, CT_Rehearsal   VARCHAR(MAX)
+, CT_Technology  VARCHAR(MAX)
+, CT_Website     VARCHAR(MAX)
+, CT_SocialMedia VARCHAR(MAX)
+)
+go
+
+
+DROP TABLE IF EXISTS t_Member_Roster
+go
+CREATE TABLE t_Member_Roster
+(
+  Lname          VARCHAR(MAX)
+, Fname          VARCHAR(MAX)
+, Voice          VARCHAR(MAX)
+, Email          VARCHAR(MAX)
+, Active         VARCHAR(MAX)
+, CC_Role        VARCHAR(MAX)
+, CC_YoungSinger VARCHAR(MAX)
+, HomePH         VARCHAR(MAX)
+, MobilePH       VARCHAR(MAX)
+, WorkPH         VARCHAR(MAX)
+, Address        VARCHAR(MAX)
+, Original_Phone VARCHAR(MAX)
+)
+go
+
+
+DROP TABLE IF EXISTS t_Mailing_List
+go
+CREATE TABLE t_Mailing_List
+(
+  LName  VARCHAR(MAX)
+, FName  VARCHAR(MAX)
+, Street  VARCHAR(MAX)
+, City  VARCHAR(MAX)
+, State  VARCHAR(MAX)
+, Zip  VARCHAR(MAX)
+, SOURCE_KEY  VARCHAR(MAX)
+)
+go
+
+
+
+DROP TABLE IF EXISTS t_Subscribed_Email_Audience
+go
+CREATE TABLE t_Subscribed_Email_Audience
+(
+  Email          VARCHAR(MAX)
+, FName          VARCHAR(MAX)
+, LName          VARCHAR(MAX)
+, Address_line_1 VARCHAR(MAX)
+, Address_line_2 VARCHAR(MAX)
+, City           VARCHAR(MAX)
+, State          VARCHAR(MAX)
+, Zip_code       VARCHAR(MAX)
+, Website        VARCHAR(MAX)
+, EMAIL_TYPE     VARCHAR(MAX)
+, MEMBER_RATING  VARCHAR(MAX)
+, OPTIN_TIME     VARCHAR(MAX)
+, OPTIN_IP       VARCHAR(MAX)
+, CONFIRM_TIME   VARCHAR(MAX)
+, CONFIRM_IP     VARCHAR(MAX)
+, GMTOFF         VARCHAR(MAX)
+, DSTOFF         VARCHAR(MAX)
+, TIMEZONE       VARCHAR(MAX)
+, CC             VARCHAR(MAX)
+, REGION         VARCHAR(MAX)
+, LAST_CHANGED   VARCHAR(MAX)
+, LEID           VARCHAR(MAX)
+, EUID           VARCHAR(MAX)
+, NOTES          VARCHAR(MAX)
+, TAGS           VARCHAR(MAX)
+)
+go
